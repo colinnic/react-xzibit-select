9917e402eb4e75ccb9dab9119b80c208ab002af5
Andrew Blowe <andrewblowe@Andrews-MacBook-Pro.local>
2015-05-20 14:49:44 -0400

    added lazy render

--- lib/components/option-list.jsx
+++ lib/components/option-list.jsx
@@ -1,19 +1,29 @@
 var React = require("react/addons");
 var types = React.PropTypes;
 var OptionListItem = require("./option-list-item.jsx");
-
+var LazyRender = require("react-lazy-render");
 
 module.exports = React.createClass({
   propTypes: {
   	options: types.array,
   	onClick: types.func
   },
+
   render: function() {
   	var optionItems = this.props.options.map(function(opt){
   		return (<OptionListItem onClick={this.props.onClick} value={opt.value} label={opt.label} />);
   	}.bind(this));
-    return <ul className="rvs-option-list">
-    		{optionItems}
-    	   </ul>;
+
+    if (optionItems.length === 0) {
+      optionItems = [(<li>Loading...</li>)];
+    }
+
+    return (
+      <ul className="rvs-option-list">
+    		  <LazyRender maxHeight={500}>
+            {optionItems}
+          </LazyRender>
+    	</ul>
+    );
   }
 });
