d6f88eecd3e79a1f645713079e6ded0ab01b68ab
Andrew Blowe <tehandyb@gmail.com>
2015-05-26 12:01:47 -0400

    fixed indentation and added addAllLimit to allow not showing the add all option

--- lib/components/option-list.jsx
+++ lib/components/option-list.jsx
@@ -5,34 +5,34 @@ var LazyRender = require("react-lazy-render");
 var ReactSizeBox = require("react-sizebox");
 
 module.exports = React.createClass({
-  propTypes: {
-  	options: types.array,
-  	onClick: types.func,
-    addAll: types.bool,
-    addAllFunc: types.func
-  },
-  render: function() {
-  	var optionItems = this.props.options.map(function(opt){
-  		return (<OptionListItem onClick={this.props.onClick} value={opt.value} label={opt.label} />);
-  	}.bind(this));
+	propTypes: {
+		options: types.array,
+		onClick: types.func,
+		addAll: types.bool,
+		addAllFunc: types.func
+	},
+	render: function() {
+		var optionItems = this.props.options.map(function(opt){
+			return (<OptionListItem onClick={this.props.onClick} value={opt.value} label={opt.label} />);
+		}.bind(this));
 
-    if (optionItems.length === 0)
-      optionItems = [(<li>None Found</li>)];
-    else if(this.props.addAll) {
-      var addAllOption = (
-        <OptionListItem addAll={this.props.addAll} onClick={this.props.addAllFunc} value={"Add All"} label="Add All"/>
-      );
-      optionItems.splice(0, 0, addAllOption);
-    }
+		if (optionItems.length === 0)
+			optionItems = [(<li>None Found</li>)];
+		else if(this.props.addAll) {
+			var addAllOption = (
+				<OptionListItem addAll={this.props.addAll} onClick={this.props.addAllFunc} value={"Add All"} label="Add All"/>
+			);
+			optionItems.splice(0, 0, addAllOption);
+		}
 
-    return (
-      <div className="content">
-        <ReactSizeBox className="overflow-y rsx-SizeBox" heightProp="maxHeight">
-    		  <LazyRender className="rxs-option-list rsx-lazyRender">
-            {optionItems}
-          </LazyRender>
-        </ReactSizeBox>
-      </div>
-    );
-  }
+		return (
+			<div className="content">
+				<ReactSizeBox className="overflow-y rsx-SizeBox" heightProp="maxHeight">
+					<LazyRender className="rxs-option-list rsx-lazyRender">
+						{optionItems}
+					</LazyRender>
+				</ReactSizeBox>
+			</div>
+		);
+	}
 });
