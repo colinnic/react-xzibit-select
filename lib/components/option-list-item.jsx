b2874169da1ac7c489f62c548aa42d5411faa3a0
Stephen Smith <stephensmith@Stephens-MacBook-Pro.local>
2015-08-28 17:11:53 -0400

    Added mobile 'pivot' for tooltips
    If the window is <= 740px wide, a skylight is used instead of tooltips

--- lib/components/option-list-item.jsx
+++ lib/components/option-list-item.jsx
@@ -1,5 +1,7 @@
 var React = require("react/addons");
 var Opentip = require('opentip');
+var SkyLight = require('react-skylight');
+var IsMobileMixin = require('../mixins/IsMobileMixin.jsx');
 require('opentip/css/opentip.css');
 
 var OptionListItem = React.createClass({
@@ -29,36 +31,81 @@ var OptionListItem = React.createClass({
       }
     };
   },
+  mixins: [IsMobileMixin],
   handleClick: function(){
     this.props.onClick(this.props.value);
   },
   createTooltip: function(component) {
-    if(component === null) {
+    if(component === null
+    || this.tooltip
+    || this.isMobile()) {
       return;
     }
 
-    if(component.tooltip) {
-      return;
-    }
-
-    component.tooltip = new Opentip(React.findDOMNode(component), this.props.toolTipContent, this.props.toolTipTitle, this.props.openTipOptions);
+    this.tooltip = new Opentip(React.findDOMNode(component), this.props.toolTipContent, this.props.toolTipTitle, this.props.openTipOptions);
   },
-  //needed for mobile to be able to show tooltips on mobile
-  blockEvent: function(event) {
+  componentDidUpdate: function() {
+    //handles mouse-based weirdness, mostly for testing.
+    if(this.tooltip) {
+      if(this.isMobile()) {
+        this.tooltip.deactivate();
+      } else {
+        this.tooltip.activate();
+      }
+    }
+  },
+  onClick: function(event) {
+    //Show tooltip only on mobile
+    if(!this.isMobile()) {
+      return;
+    }
+
+    this.refs.tooltip.show();
+
     event.preventDefault();
     event.stopPropagation();
   },
   render: function() {
-    var className = "rxs-option-list-item", hoverIcon = "";
+    var className = "rxs-option-list-item",
+    hoverIcon = null,
+    skyLight = null;
 
-    if(this.props.addAll)
+    if(this.props.addAll) {
       className += " add-all";
+    }
+
     if(this.props.toolTipContent && this.props.toolTipContent !== "") {
       hoverIcon = (
-        <div className="hover-icon" onClick={this.blockEvent} ref={this.createTooltip}>i</div>
+        <div
+          className="hover-icon"
+          onClick={this.onClick}
+          ref={this.createTooltip}>
+          i
+        </div>
       );
+
+      if(this.isMobile()) {
+        skyLight = (
+          <SkyLight
+            ref="tooltip"
+            title={this.props.tooltipTitle}>
+            {this.props.toolTipContent}
+          </SkyLight>
+        );
+      }
     }
-    return <div className={className}><button className="rxs-option-button" onClick={this.handleClick}>{this.props.label}{hoverIcon}</button></div>;
+
+    return (
+      <div className={className}>
+        <button
+          className="rxs-option-button"
+          onClick={this.handleClick}>
+          {this.props.label}
+          {hoverIcon}
+        </button>
+        {skyLight}
+      </div>
+    );
   }
 });
 
